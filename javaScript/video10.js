a98dd5e91f52cbfa6005b09e14eac5f1b04e7e24
JahnviJain <138013346+JahnviJain@users.noreply.github.com>
2024-06-11 14:46:28 +0000

    commit

---
+++ javaScript/video10.js
@@ -0,0 +1,17 @@
+let myYoutubeName = "jj@google.com"
+
+let anotherName = myYoutubeName
+anotherName = "jahnvi@hotmail.com"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userone = {
+    email: "jj@gmail.com",
+    upi:456789
+}
+let usertwo = userone
+usertwo.email = "jahnvi@hotmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
