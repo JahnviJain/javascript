51436b94e0bc80d8248674f03d9cdb6908256f51
JahnviJain <138013346+JahnviJain@users.noreply.github.com>
2024-06-11 12:13:00 +0000

    commit

---
+++ javaScript/video5.js
@@ -0,0 +1,21 @@
+"use strict" //treat all java code as new version
+
+//alert(3 + 3) //not used on nodejs, only on browser
+
+console.log(3 + 3);
+
+let name = "jahnvi"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(undefined); //undefined
+console.log(null); //null
